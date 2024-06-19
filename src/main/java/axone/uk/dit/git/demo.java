0de6bef40fd563f4023cbc702ffb5799f9aa0df7
Sountharya <sountharya.mani96@gmail.com>
2024-06-19 17:37:04 +0100

    JIRA-145 - changes for payment scripts

---
+++ src/main/java/axone/uk/dit/git/demo.java
@@ -0,0 +1,8 @@
+package axone.uk.dit.git;
+
+public class demo {
+
+    public void methodone(){
+        System.out.println("This is method one");
+    }
+}
---
+++ src/test/java/axone/uk/dit/demotest.java
@@ -0,0 +1,11 @@
+package axone.uk.dit;
+
+import org.testng.annotations.Test;
+
+public class demotest {
+
+    @Test
+    public void testmethod(){
+        System.out.println("TestMethod one");
+    }
+}
